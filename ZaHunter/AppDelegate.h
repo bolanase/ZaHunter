d08d8273457d73de89006fd88e7e1e9d313e0cd8
Tony Severino <tony@professional-painters.com>
2014-01-22 14:04:37 -0600

    Initial Commit

---
+++ ZaHunter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ZaHunter
+//
+//  Created by Anthony  Severino on 1/22/14.
+//  Copyright (c) 2014 Simple Management Solutions, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZaHunter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ZaHunter
+//
+//  Created by Anthony  Severino on 1/22/14.
+//  Copyright (c) 2014 Simple Management Solutions, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
